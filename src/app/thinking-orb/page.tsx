d848022b54fb71d5e7d214e19c458aaa968f40c0
Damian Izaguirre <damianizaguirre@Damians-MacBook-Pro-2.local>
2026-07-21 23:25:04 -0500

    Make ThinkingOrb iframe background transparent

--- src/app/thinking-orb/page.tsx
+++ src/app/thinking-orb/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { useEffect } from "react";
 import { ThinkingOrb } from "thinking-orbs";
 
 export default function ThinkingOrbFrame() {
+  useEffect(() => {
+    document.documentElement.style.background = "transparent";
+    document.body.style.margin = "0";
+    document.body.style.minHeight = "64px";
+    document.body.style.background = "transparent";
+  }, []);
+
   return <ThinkingOrb state="composing" size={64} />;
 }
